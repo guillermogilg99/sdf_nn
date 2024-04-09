fcd67b87d78534135bfa457be504a9348b0f636c
guillermogilg99 <guillermogilg99@gmail.com>
2024-04-09 12:07:13 +0200

    Minor update

--- scripts/siren_sdf.py
+++ scripts/siren_sdf.py
@@ -22,7 +22,7 @@ import torch.nn as nn
 #from torch.utils.data import DataLoader, TensorDataset
 #from torchvision import datasets, transforms
 
-import plotly.graph_objs as go
+#import plotly.graph_objs as go
 
 # Custom includes
 
